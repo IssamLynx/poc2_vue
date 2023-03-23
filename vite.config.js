45cf6d2aa1960b832653859b015c71cd4885bf4e
edouardglx <edouard.gloux@zenika.com>
2023-03-23 14:09:47 +0100

    config vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/poc-vue3/',
+  base: '/pocvue3/',
   plugins: [vue()],
   resolve: {
     alias: {
