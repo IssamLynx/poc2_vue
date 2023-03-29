86590ffb76c73aeba40059adabd4e14339f88f05
edouardglx <edouard.gloux@zenika.com>
2023-03-29 12:48:18 +0200

    Changed beerstate state in animestate

--- src/components/CardDetails/DetailsComponent.vue
+++ src/components/CardDetails/DetailsComponent.vue
@@ -8,7 +8,7 @@ console.log(route.params.id)
 
 //STATE
 const state = reactive({
-  beerDetails: []
+  animeDetails: []
 })
 
 //OPTION
@@ -20,7 +20,7 @@ const options = {
   }
 }
 //API CALL
-async function fetchBeerdetails() {
+async function fetchAnimedetails() {
   const response = await fetch(
     `https://anime-db.p.rapidapi.com/anime/by-id/${route.params.id}`,
     options
@@ -28,21 +28,21 @@ async function fetchBeerdetails() {
   return response
 }
 
-state.beerDetails = await fetchBeerdetails()
+state.animeDetails = await fetchAnimedetails()
 </script>
 
 <template>
   <div class="h-screen">
     <div class="w-full flex space-x-4 p-4">
       <img
-        :src="state.beerDetails.image"
-        alt="{{state.beerDetails.image}}"
+        :src="state.animeDetails.image"
+        alt="{{state.animeDetails.image}}"
         class="h-full aspect-auto"
       />
       <div class="flex-col">
-        <h1 class="font-bold text-xl">{{ state.beerDetails.title }}</h1>
-        <p class="my-4 text-justify" v-if="state.beerDetails.synopsis">
-          {{ state.beerDetails.synopsis }}
+        <h1 class="font-bold text-xl">{{ state.animeDetails.title }}</h1>
+        <p class="my-4 text-justify" v-if="state.animeDetails.synopsis">
+          {{ state.animeDetails.synopsis }}
         </p>
         <p class="my-4 text-justify" v-else>Pas de synopsis disponible</p>
       </div>
