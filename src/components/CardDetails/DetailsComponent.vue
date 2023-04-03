e992c915fac6986066d390155f15863cde34c39c
Ludovic <ludovicli973@gmail.com>
2023-04-03 15:29:44 +0200

    Refactored the Details page to make it more clean

--- src/components/CardDetails/DetailsComponent.vue
+++ src/components/CardDetails/DetailsComponent.vue
@@ -1,41 +1,31 @@
 <script setup>
-import { reactive } from 'vue'
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { getSpecificAnime } from '../../services/api'
 
-//PARAMS
 const route = useRoute()
-console.log(route.params.id)
-
-//STATE
-const state = reactive({
-  animeDetails: []
-})
+const id = route.params.id
+const animeDetails = ref({})
 
 const fetchSpecificAnime = async () => {
   try {
-    const specificAnime = await getSpecificAnime(route.params.id)
-    state.animeDetails = specificAnime
+    const specificAnime = await getSpecificAnime(id)
+    animeDetails.value = specificAnime
   } catch (error) {
     console.error(error.message)
   }
 }
-
 await fetchSpecificAnime()
 </script>
 
 <template>
   <div class="h-screen">
     <div class="w-full flex space-x-4 p-4">
-      <img
-        :src="state.animeDetails.image"
-        alt="{{state.animeDetails.image}}"
-        class="h-full aspect-auto"
-      />
+      <img :src="animeDetails.image" alt="{{animeDetails.image}}" class="h-full aspect-auto" />
       <div class="flex-col">
-        <h1 class="font-bold text-xl">{{ state.animeDetails.title }}</h1>
-        <p class="my-4 text-justify" v-if="state.animeDetails.synopsis">
-          {{ state.animeDetails.synopsis }}
+        <h1 class="font-bold text-xl">{{ animeDetails.title }}</h1>
+        <p class="my-4 text-justify" v-if="animeDetails.synopsis">
+          {{ animeDetails.synopsis }}
         </p>
         <p class="my-4 text-justify" v-else>Pas de synopsis disponible</p>
       </div>
