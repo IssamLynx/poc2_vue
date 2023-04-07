ff6fc26c57b408de846408d094eb26116e323cf0
Ludovic <ludovicli973@gmail.com>
2023-04-07 15:05:29 +0200

    Created layout file with header and footer

---
+++ src/layouts/defaultLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Header from '../components/Header/Header.vue'
+import Footer from '../components/Footer/Footer.vue'
+</script>
+
+<template>
+  <Header />
+  <slot />
+  <Footer />
+</template>
