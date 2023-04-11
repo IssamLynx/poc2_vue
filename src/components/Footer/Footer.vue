6f153e3b5491d67f5b9cb62b8c5a41005a2bba8e
Ludovic <ludovicli973@gmail.com>
2023-04-11 11:54:52 +0200

    Replaced div tags with more meaningful ones

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,15 +1,15 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-  <div class="h-24 w-screen box-border flex justify-around items-center flex-col">
-    <ul class="w-1/2 text-black flex justify-around justify-items-center">
-      <li><a>Youtube</a></li>
-      <li><a>Instagram</a></li>
-      <li><a>Facebook</a></li>
-      <li><a>Twitter</a></li>
-    </ul>
+  <footer class="h-24 w-screen box-border flex justify-around items-center flex-col">
+    <nav class="w-1/2 text-black flex justify-around justify-items-center">
+      <RouterLink to="/">Youtube</RouterLink>
+      <RouterLink to="/">Instagram</RouterLink>
+      <RouterLink to="/">Facebook</RouterLink>
+      <RouterLink to="/">Twitter</RouterLink>
+    </nav>
     <p class="text-black">Copyright © 2023 Zenika − Tous droits réservés.</p>
-  </div>
+  </footer>
 </template>
-
-<style scoped></style>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,26 +1,16 @@
 <script setup>
-import Logo from '../../assets/zenika_logo.png'
+import Logo from '@/assets/zenika_logo.png'
 import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div class="w-full h-16 flex items-center space-x-4">
-    <RouterLink to="/">
-      <img :src="Logo" class="h-full aspect-auto w-[90px]" />
-    </RouterLink>
-    <ul>
-      <li><a>Valeurs</a></li>
-    </ul>
-    <ul>
-      <li><a>Offres</a></li>
-    </ul>
-    <ul>
-      <li><a>Green IT</a></li>
-    </ul>
-    <ul>
-      <li><a>Sécurité</a></li>
-    </ul>
-  </div>
+  <header>
+    <nav class="w-full h-16 flex items-center space-x-4">
+      <RouterLink to="/"><img :src="Logo" class="h-full aspect-auto w-[90px]" /></RouterLink>
+      <RouterLink to="/">Valeurs</RouterLink>
+      <RouterLink to="/">Offres</RouterLink>
+      <RouterLink to="/">Green IT</RouterLink>
+      <RouterLink to="/">Sécurité</RouterLink>
+    </nav>
+  </header>
 </template>
-
-<style scoped></style>
