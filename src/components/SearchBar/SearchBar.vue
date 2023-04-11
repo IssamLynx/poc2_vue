26ea182b73332ac1716f5b2c3a552c1347ceb04f
Ludovic <ludovicli973@gmail.com>
2023-04-11 11:58:11 +0200

    Modified the searchbar component in order to do a v-model

--- src/components/SearchBar/SearchBar.vue
+++ src/components/SearchBar/SearchBar.vue
@@ -1,37 +1,16 @@
 <script setup>
-import { ref } from 'vue'
-
-const emit = defineEmits(['handleSearch', 'handleClear'])
-let searchInput = ref('')
-
-const handleClear = () => {
-  searchInput.value = ''
-  emit('handleClear')
-}
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
 </script>
 
 <template>
   <div class="flex items-center mt-2 ml-4">
     <input
       type="text"
-      v-model="searchInput"
       class="w-[200px] h-[30px] border border-black rounded"
       placeholder="Search"
+      :value="props.modelValue"
+      @input="emit('update:modelValue', $event.target.value)"
     />
-    <button
-      @click="emit('handleSearch', searchInput)"
-      class="ml-[10px] w-[100px] h-[30px] border border-black rounded bg-black text-white cursor-pointer"
-    >
-      Search
-    </button>
-
-    <button
-      v-if="searchInput.length > 0"
-      @click="handleClear"
-      class="ml-[10px] w-[100px] h-[30px] border rounded bg-gray-200 border-gray-200"
-      role="clear_button"
-    >
-      Clear
-    </button>
   </div>
 </template>
