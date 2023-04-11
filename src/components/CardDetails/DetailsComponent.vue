1a2f5613280542d2b0aa23881a1da85008b1c6c1
edouardglx <edouard.gloux@zenika.com>
2023-04-11 16:48:36 +0200

    Created an error management for the image in detail component

--- src/components/CardDetails/DetailsComponent.vue
+++ src/components/CardDetails/DetailsComponent.vue
@@ -1,11 +1,20 @@
 <script setup>
+import noImageAvailable from '@/assets/SVG/no_image_available.svg'
 const props = defineProps(['title', 'image', 'synopsis'])
+const handleImageError = (event) => {
+  event.target.src = noImageAvailable
+}
 </script>
 
 <template>
   <div class="h-screen">
     <div class="w-full flex space-x-4 p-4">
-      <img :src="props.image" alt="{{props.image}}" class="h-full aspect-auto" />
+      <img
+        :src="props.image"
+        alt="{{props.image}}"
+        class="h-full aspect-auto"
+        @error="handleImageError"
+      />
       <div class="flex-col">
         <h1 class="font-bold text-xl">{{ props.title }}</h1>
         <p class="my-4 text-justify">
