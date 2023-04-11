e8cbd575e02a95981db89c2579f5a73d978f8f37
Ludovic <ludovicli973@gmail.com>
2023-04-11 11:59:34 +0200

    Refactored the API file with async/await and API link in .env file

--- src/services/api.js
+++ src/services/api.js
@@ -7,17 +7,19 @@ const options = {
 }
 
 const fetchData = async (link) => {
-  return await fetch(link, options).then((res) => res.json())
+  const response = await fetch(link, options)
+  const data = await response.json()
+  return data
 }
 
 export const getAllAnime = async (page = 1, size = 30) => {
   const response = await fetchData(
-    `https://anime-db.p.rapidapi.com/anime?page=${page}&size=${size}`
+    `${import.meta.env.VITE_APP_API_LINK_ALL_ANIME}?page=${page}&size=${size}`
   )
   return response.data
 }
 
 export const getSpecificAnime = async (animeId) => {
-  const response = await fetchData(`https://anime-db.p.rapidapi.com/anime/by-id/${animeId}`)
+  const response = await fetchData(`${import.meta.env.VITE_APP_API_LINK_SPECIFIC_ANIME}/${animeId}`)
   return response
 }
