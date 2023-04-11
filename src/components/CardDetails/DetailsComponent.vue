850577627603762635a885f52dd8159e332d13fb
Ludovic <ludovicli973@gmail.com>
2023-04-11 14:10:51 +0200

    Removed the API call from the component detailsComponent and moved it to the detailsPage

--- src/components/CardDetails/DetailsComponent.vue
+++ src/components/CardDetails/DetailsComponent.vue
@@ -1,33 +1,16 @@
 <script setup>
-import { ref } from 'vue'
-import { useRoute } from 'vue-router'
-import { getSpecificAnime } from '@/services/api'
-
-const route = useRoute()
-const id = route.params.id
-const animeDetails = ref({})
-
-const fetchSpecificAnime = async () => {
-  try {
-    const specificAnime = await getSpecificAnime(id)
-    animeDetails.value = specificAnime
-  } catch (error) {
-    console.error(error.message)
-  }
-}
-await fetchSpecificAnime()
+const props = defineProps(['title', 'image', 'synopsis'])
 </script>
 
 <template>
   <div class="h-screen">
     <div class="w-full flex space-x-4 p-4">
-      <img :src="animeDetails.image" alt="{{animeDetails.image}}" class="h-full aspect-auto" />
+      <img :src="props.image" alt="{{props.image}}" class="h-full aspect-auto" />
       <div class="flex-col">
-        <h1 class="font-bold text-xl">{{ animeDetails.title }}</h1>
-        <p class="my-4 text-justify" v-if="animeDetails.synopsis">
-          {{ animeDetails.synopsis }}
+        <h1 class="font-bold text-xl">{{ props.title }}</h1>
+        <p class="my-4 text-justify">
+          {{ props.synopsis }}
         </p>
-        <p class="my-4 text-justify" v-else>Pas de synopsis disponible</p>
       </div>
     </div>
   </div>
--- src/views/DetailsPage/DetailPage.vue
+++ src/views/DetailsPage/DetailPage.vue
@@ -1,13 +1,48 @@
 <script setup>
+import { ref, computed } from 'vue'
 import DetailsComponent from '@/components/CardDetails/DetailsComponent.vue'
 import defaultLayout from '@/layouts/defaultLayout.vue'
+import { useRoute } from 'vue-router'
+import { getSpecificAnime } from '@/services/api'
+
+const route = useRoute()
+const id = route.params.id
+const anime = ref({})
+const isLoading = ref(true)
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0
+}
+
+const fetchSpecificAnime = async () => {
+  try {
+    const specificAnime = await getSpecificAnime(id)
+    anime.value = specificAnime
+  } catch (error) {
+    console.error(error.message)
+  } finally {
+    isLoading.value = false
+  }
+}
+
+fetchSpecificAnime()
+
+const animeSynopsis = computed(() => {
+  return anime.value.synopsis || 'Pas de synopsis disponible'
+})
 </script>
 
 <template>
   <defaultLayout>
-    <Suspense>
-      <DetailsComponent />
-      <template v-slot:fallback>En cours de chargement ...</template>
-    </Suspense>
+    <p v-if="isLoading">En cours de chargement...</p>
+    <div v-else>
+      <DetailsComponent
+        v-if="!isEmpty(anime)"
+        :title="anime.title"
+        :image="anime.image"
+        :synopsis="animeSynopsis"
+      />
+      <p v-else>Not found</p>
+    </div>
   </defaultLayout>
 </template>
