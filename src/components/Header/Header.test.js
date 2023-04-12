603a8346285c8aa580ad149fc6a9a55450bff154
Ludovic <ludovicli973@gmail.com>
2023-04-12 12:21:08 +0200

    Corrected test files in order to expect routerlink tags

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,14 +1,14 @@
 import { render } from '@testing-library/vue'
-import { expect, test, vi } from 'vitest'
+import { test } from 'vitest'
 import Header from './Header.vue'
 import '@testing-library/jest-dom'
-
-vi.mock('vue-router', () => ({
-  RouterLink: vi.fn()
-}))
+import router from '@/router'
 
 test('header has the text "Valeurs"', async () => {
-  const { getByText } = render(Header)
-  const elem = getByText('Valeurs')
-  expect(elem).toBeInTheDocument()
+  const { getByRole } = render(Header, {
+    global: {
+      plugins: [router]
+    }
+  })
+  getByRole('link', { name: 'Valeurs' })
 })
